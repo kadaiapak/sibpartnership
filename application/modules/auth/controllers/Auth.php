befda3cf7f93c2c667d3103145a5c3ed8e4a4ffa
Denisuardi <denisuardi05@gmail.com>
2022-04-11 12:05:37 +0700

    menghapus upload

--- application/modules/auth/controllers/Auth.php
+++ application/modules/auth/controllers/Auth.php
@@ -67,10 +67,9 @@ class Auth extends CI_Controller
         $this->session->unset_userdata('username');
         $this->session->unset_userdata('role_id');
 
-         $this->session->set_flashdata('message', 
+        $this->session->set_flashdata('message', 
         "<div class='alert alert-success' role='alert' >You have been logout</div>");
         redirect('auth');
-
     }
 
     public function blocked()
